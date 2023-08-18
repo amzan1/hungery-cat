0d9fb7a0862d2b89cdb7d02afe1d6165f4771fa4
amzan1 <amzanabdullah97@gmail.com>
2023-08-19 00:33:52 +0300

    level 1 complete

--- main.js
+++ main.js
@@ -3,17 +3,22 @@ let mycat;
 let hidebtn = document.getElementById("hide");
 function startGame(){
     hidebtn.style.display = "none"
-    mycat =  new component(140, 90, "cat.gif", 0, 190, "image");
+    mycat =  new component(280, 180, "cat.gif", 0, 90, "image");
+    food = new component(140, 110, "catFood.png", 760, 500, "image");
+    ground = new component(1100, 140, "grass-sm.png", -9, 500, "image");
+    //mycat =  new component(w, h, color, x, y, "image");
+
 
     myGameArea.start();
 }
 let canvas = document.createElement("canvas");
+
 let area = document.getElementById("area");
 var myGameArea = {
     canvas : canvas,
     start : function() {
-        this.canvas.width = innerWidth;
-        this.canvas.height = innerHeight;
+        this.canvas.width = 1000;
+        this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         area.appendChild(this.canvas);
         this.int = setInterval(updateGame , 10);
@@ -25,7 +30,7 @@ var myGameArea = {
     
     }
 function component(w, h, color, x, y, type){
-    const gravity = 2.5;
+    const gravity = 0.5;
     this.width = w;
     this.height = h;
     this.x = x;
@@ -65,30 +70,74 @@ this.update = function() {
     }
 
 }
+const k = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 function updateGame(){
     myGameArea.clear();
     mycat.position();
+    ground.update();
+    food.update();
     mycat.update();
+    if(k.right.pressed && mycat.x<=750){
+        mycat.move_x = 5
+    }else if(k.left.pressed && mycat.x >10){
+        mycat.move_x = -5
+
+    }else {
+        mycat.move_x = 0;
+    }
+    if(mycat.x==food.x && k.right.pressed ){
+        alert("you win");
+        window.location.href="level2.html"
+    }
 }
+
 // moving the cat
 /////////// start ///////////
 
 addEventListener("keydown", function(event){
-    if(event.key=="ArrowRight"){
+    if(event.key == "ArrowRight"){
         mycat.move_x = 1; 
+        k.right.pressed=true
+        // console.log("right")
     }
-    if(event.key=="ArrowLeft"){
+    if(event.key == "ArrowLeft"){
         mycat.move_x = -1; 
+        k.left.pressed=true
+        // console.log("left")
     }
-    console.log(event);
+    if(event.key == " "){
+        mycat.move_y-= 10;
+    }
+    // console.log(k.right.pressed);
+    // console.log(event)
 })
 addEventListener("keyup", function(event){
     if(event.key=="ArrowRight"){
         mycat.move_x = 0; 
+        k.right.pressed=false
+        // console.log("right")
     }
     if(event.key=="ArrowLeft"){
         mycat.move_x = 0; 
+        k.left.pressed=false
+        // console.log("left")
     }
+    if(event.key == " "){
+        mycat.move_y-= 0;
+    }
+    // console.log(k.right.pressed);
+
 })
 /////////// end ////////////
 
+// 
+/////////// start ///////////
+
+/////////// end ////////////
